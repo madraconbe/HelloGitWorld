9100af8d52e3c106d6c5e7b0501e25816bb254ce
David Russell <david.russell@lpa.com>
2012-03-02 09:25:48 -0500

    Added another line

--- src/com/lpa/hello/HelloGit.java
+++ src/com/lpa/hello/HelloGit.java
@@ -8,6 +8,7 @@ public class HelloGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello, Git!");
+		System.out.println("We really should stop meeting this way.");
 	}
 
 }
